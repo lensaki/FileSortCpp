a1872991341be7902954dca74b5f95f105cf9d99
Lorenzo Buda <70598755+Xayger@users.noreply.github.com>
2026-05-07 15:17:04 -0700

    Every file type has a vector it can be stored in sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -85,7 +85,28 @@ void FilesDivider(vector<string> files) //Parses each element of the vector and 
     string fileType;
     string fileinquestion;
 
-    vector<string> fileTypevec;
+    vector<string> docx;
+    vector<string> txt;
+    vector<string> xlsx;
+    vector<string> json;
+    vector<string> h;
+    vector<string> pdf;
+    vector<string> svg;
+    vector<string> cpp;
+    vector<string> back;
+    vector<string> jpg;
+    vector<string> png;
+    vector<string> csv;
+    vector<string> pptx;
+    vector<string> tmp;
+    vector<string> README;
+    vector<string> py;
+    vector<string> yaml;
+    vector<string> mp3;
+    vector<string> mp4;
+    vector<string> css;
+    vector<string> md;
+    vector<string> hmtl;
     vector<string> overflow;
 
     for (int pos = 0; pos < files.size(); pos++)
@@ -106,27 +127,161 @@ void FilesDivider(vector<string> files) //Parses each element of the vector and 
         {
 
             cout << fileinquestion << " would go in folder : " << filename << endl;
-            fileTypevec.push_back(fileinquestion);
-            //for (int i = 1; i < fileType.size(); i++)
-            //{
-            //    cout << files[i] << "\n";  // print each file from the vector
-            //}
-
+            docx.push_back(fileinquestion);
             cout << "Added: " << fileinquestion << " to " << filename << endl;
             cout << endl;
-
         }
         
-        else if (filename == ".txt") {
-
+        else if (filename == ".txt") 
+        {
             cout << files.at(pos) << " would go in folder : " << filename << endl;
-            fileTypevec.push_back(files.at(pos));
-
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
         }
-
-        else {
+        else if (filename == ".xlsx")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".json")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".csv")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".h")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".pdf")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".svg")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".cpp")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".bak")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".png")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".jpg")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".pptx")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".tmp")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".README")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".py")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".yaml")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".mp3")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".mp4")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".css")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".md")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else if (filename == ".html")
+        {
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            txt.push_back(files.at(pos));
+            cout << "Added: " << fileinquestion << " to " << filename << endl;
+            cout << endl;
+        }
+        else 
+        {
             cout << files.at(pos) << " was not put in a folder" << endl;
-
         }
 
     }
