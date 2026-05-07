f12baf9ff73b6cb38d8348ab56ddadbd6d495aa5
Len Sakimukai <lensouthbay@gmail.com>
2026-05-07 13:58:10 -0700

    Work in progress for new vector depending on file type

--- sort.cpp
+++ sort.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 /*
-* Wanted to split the main and the other methods between two different cpp files 
+* Wanted to split the main and the other methods between two different cpp files
 * but it was having problems with the variables for the vector and file Directory
 */
 
 vector<string> listFiles(string path); // returns vector
 bool checkDirectory(string path); //checks if Directory is valid
-void FileDivider(vector<string> files); //takes in the main vector of files and divides it into sub-vectors
+void FilesDivider(vector<string> files); //takes in the main vector of files and divides it into sub-vectors
 
 int main()
 {
@@ -28,10 +28,13 @@ int main()
 
     vector<string> files = listFiles(path);  // receive the vector from listFiles
 
-    for (int i = 0; i < files.size(); i++)
+    for (int i = 2; i < files.size(); i++)
     {
         cout << files[i] << "\n";  // print each file from the vector
     }
+    cout << endl;
+    cout << endl;
+    FilesDivider(files);
 
     return 0;
 }
@@ -57,7 +60,7 @@ vector<string> listFiles(string path)  // Parses through the directory and popul
     return files;  // return the completed vector
 }
 
-bool checkDirectory(string path) //checks if a valid Direcotry has been inserted
+bool checkDirectory(string path) //checks if a valid Directory has been inserted
 {
     WIN32_FIND_DATAA findData;
     HANDLE hFind;
@@ -75,12 +78,50 @@ bool checkDirectory(string path) //checks if a valid Direcotry has been inserted
 
 void FilesDivider(vector<string> files) //Parses each element of the vector and populates new vectors based on the file type
 {
-    int period;
+    string::size_type period;
+    string filename;
     string fileType;
-    for (int pos = 0; pos < files.size(); pos++)
+	string fileinquestion;
+
+    for (int pos = 2; pos < files.size(); pos++)
     {
-        period = files.at(pos).find(".");
-        fileType = files.at(pos).substr(period, -2);
+        period = files.at(pos).rfind(".");
+        fileType = files.at(pos).substr(period);
+        filename = fileType;
+
+        fileinquestion = files.at(pos);
+
+
+        vector<string> fileTypevec;
+
+        if (filename == ".docx"){
+
+            cout << fileinquestion << " would go in folder : " << filename << endl;
+            fileTypevec.push_back(fileinquestion);
+            //for (int i = 1; i < fileType.size(); i++)
+            //{
+            //    cout << files[i] << "\n";  // print each file from the vector
+            //}
+
+            cout << "Added: " << fileinquestion << " to " << filename <<endl;
+            cout << endl;
+         
+           
+
+
+		}
+        else if (filename == ".txt") {
+
+            cout << files.at(pos) << " would go in folder : " << filename << endl;
+            fileTypevec.push_back(files.at(pos));
+   
+        }
+
+        else {
+            cout << files.at(pos) << " was not put in a folder" << endl;
+
+        }
+
     }
     //missing populates new vectors based on file types
 }
