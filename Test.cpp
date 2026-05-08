9bd4d4546822c10ed12add1b5dd4a50ab1bce7d0
Lorenzo Buda <70598755+Xayger@users.noreply.github.com>
2026-05-07 17:48:21 -0700

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,25 @@
+#include "Sort.h"
+
+int main()
+{
+    string path;
+    bool check = false;
+    while (check != true)
+    {
+        cout << "Enter path to list files: ";
+        cin >> path;
+        check = checkDirectory(path);
+    }
+
+    vector<string> files = listFiles(path);  // receive the vector from listFiles
+
+    for (int i = 2; i < files.size(); i++)
+    {
+        cout << files[i] << "\n";  // print each file from the vector
+    }
+    cout << endl;
+    cout << endl;
+    FilesDivider(files);
+
+    return 0;
+}
