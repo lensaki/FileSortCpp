c132b88dd7841e89b2edbc2778d76b44635b8111
Len Sakimukai <lensouthbay@gmail.com>
2026-05-10 00:43:43 -0700

    updated file divider

--- Sort.h
+++ Sort.h
@@ -6,11 +6,15 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include <filesystem>
+
 
 using namespace std;
+namespace fs = std::filesystem;
 
 vector<string> listFiles(string path); // returns vector
 bool checkDirectory(string path); //checks if Directory is valid
-void FilesDivider(vector<string> files); //takes in the main vector of files and divides it into sub-vectors
+void FilesDivider(vector<string> files, string path); //takes in the main vector of files and divides it into sub-vectors
+void AlphabetSortFiles(vector<string> filename, string path); //sorts the files in each vector alphabetically
 	
 #endif
