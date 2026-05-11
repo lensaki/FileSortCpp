d1d91e1a3a25aba5b4c11edd38635e4cbabb7a3f
Lorenzo Buda <70598755+Xayger@users.noreply.github.com>
2026-05-10 17:09:12 -0700

    Sort.h to open files

--- Sort.h
+++ Sort.h
@@ -1,13 +1,16 @@
 #ifndef SORT
 #define SORT
 
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 #include<stdio.h>
 #include<windows.h>
 #include<iostream>
 #include<string>
 #include<vector>
 #include <filesystem>
-
+#include<iomanip>
+#include <shellapi.h>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -16,5 +19,6 @@ vector<string> listFiles(string path); // returns vector
 bool checkDirectory(string path); //checks if Directory is valid
 void FilesDivider(vector<string> files, string path); //takes in the main vector of files and divides it into sub-vectors
 void AlphabetSortFiles(vector<string> filename, string path); //sorts the files in each vector alphabetically
-	
+void UserChoise(vector<vector<string>*> names, string path);
+
 #endif
